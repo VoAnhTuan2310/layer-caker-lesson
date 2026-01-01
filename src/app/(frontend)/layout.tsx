93816d719ec702329fd86ee5abf1ef9f6d07cb07
VoAnhTuan2310 <vatuan231004@gmail.com>
2026-01-02 00:21:37 +0700

    fix: remove missing header import

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,5 @@
 import "../globals.css";
-import { Header } from '@/components/header'
+import { Header } from '@/component/header'
 import { SanityLive } from '@/sanity/lib/live';
 
 export default function FrontendLayout({
