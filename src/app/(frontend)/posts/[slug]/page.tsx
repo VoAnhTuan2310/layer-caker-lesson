ff2c8c8f86312ae6620a84c60d5fd6a61138d36b
VoAnhTuan2310 <vatuan231004@gmail.com>
2026-01-02 04:37:13 +0700

    fix code

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -16,6 +16,7 @@ export default async function Page({
   if (!post) {
     notFound()
   }
+
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <Post {...post} />
