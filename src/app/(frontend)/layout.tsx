e78d799772de8a6c8453bcaac50500b4d45c50fc
VoAnhTuan2310 <vatuan231004@gmail.com>
2026-01-01 14:01:28 +0700

    fix layout code

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,16 +1,15 @@
-git remote add origin https://github.com/VoAnhTuan2310/layer-caker-lesson.git
-git branch -M main
-git push -u origin mainimport { SanityLive } from '@/sanity/lib/live'
+import "../globals.css";
+import { SanityLive } from "@/sanity/lib/live";
 
 export default function FrontendLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <>
       {children}
       <SanityLive />
     </>
-  )
+  );
 }
